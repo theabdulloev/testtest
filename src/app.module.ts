f0065e4ff1bda1141574b0c8e0503821bccaf818
Ismoiljon Abdulloev <2022abdulloev@gmail.com>
2024-04-29 15:47:29 +0500

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,19 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
-import { join } from 'path';
-import { ServeStaticModule } from '@nestjs/serve-static';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ProductsModule } from "./products/products.module";
+import { join } from "path";
+import { ServeStaticModule } from "@nestjs/serve-static";
 // mongodb+srv://abdulloev:0CSxryhOnWJEhipW@tojikonbookcl.jsrcsga.mongodb.net/?retryWrites=true&w=majority
 @Module({
   imports: [
     ProductsModule,
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'client'),
+      rootPath: join(__dirname, "..", "client"),
     }),
-    MongooseModule.forRoot(`mongodb://127.0.0.1:27017/admin`)
+    MongooseModule.forRoot(`mongodb://127.0.0.1:27017/admin`),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
 
