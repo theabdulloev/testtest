5c9201e009e70a2dd7f002293737174b606f331b
Ismoiljon Abdulloev <2022abdulloev@gmail.com>
2024-04-29 15:51:58 +0500

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,14 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { ProductsModule } from "./products/products.module";
 import { join } from "path";
 import { ServeStaticModule } from "@nestjs/serve-static";
-// mongodb+srv://abdulloev:0CSxryhOnWJEhipW@tojikonbookcl.jsrcsga.mongodb.net/?retryWrites=true&w=majority
+// 
 @Module({
   imports: [
     ProductsModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, "..", "client"),
     }),
-    MongooseModule.forRoot(`mongodb://127.0.0.1:27017/admin`),
+    MongooseModule.forRoot(`mongodb+srv://abdulloev:0CSxryhOnWJEhipW@tojikonbookcl.jsrcsga.mongodb.net/?retryWrites=true&w=majority`),
   ],
   controllers: [],
   providers: [],
