51e01db60c72fa68f427cac16abf089314cf521e
Ismoiljon Abdulloev <2022abdulloev@gmail.com>
2024-05-02 13:27:50 +0500

    fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,16 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { ProductsModule } from "./products/products.module";
 import { join } from "path";
 import { ServeStaticModule } from "@nestjs/serve-static";
-// 
+// mongodb+srv://abdulloev:0CSxryhOnWJEhipW@tojikonbookcl.jsrcsga.mongodb.net/?retryWrites=true&w=majority
 @Module({
   imports: [
     ProductsModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, "..", "client"),
     }),
-    MongooseModule.forRoot(`mongodb+srv://abdulloev:0CSxryhOnWJEhipW@tojikonbookcl.jsrcsga.mongodb.net/?retryWrites=true&w=majority`),
+    MongooseModule.forRoot(`mongodb://127.0.0.1:27017/admin`),
   ],
   controllers: [],
   providers: [],
 })
 export class AppModule {}
-
-// git remote add origin https://github.com/theabdulloev/testtest.git
-// git branch -M main
-// git push -u origin main
--- src/products/schemas/product.schema.ts
+++ src/products/schemas/product.schema.ts
@@ -1,15 +1,39 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose'
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
 
-export type ProductDocument = Product & Document
+export type ProductDocument = Product & Document;
 
 @Schema()
 export class Product {
   @Prop()
-  title: string
+  username: string;
 
   @Prop()
-  price: number
+  vip: boolean;
+
+  @Prop()
+  fullName: string;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  watch: number;
+
+  @Prop()
+  salary: number;
+
+  @Prop()
+  contacts: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  location: string;
+
+  @Prop()
+  verification: boolean;
 }
 
-export const ProductSchema = SchemaFactory.createForClass(Product)
+export const ProductSchema = SchemaFactory.createForClass(Product);
