e71e018e9674ef30d87020e1c97f4015a190fd9b
Ismoiljon Abdulloev <2022abdulloev@gmail.com>
2024-05-04 16:22:14 +0500

    fixed

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -9,22 +9,20 @@ import {
   Param,
   Post,
   Put,
-} from '@nestjs/common';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
-import { ProductsService } from './products.service';
-import { Product } from './schemas/product.schema';
+} from "@nestjs/common";
+import { CreateProductDto } from "./dto/create-product.dto";
+import { UpdateProductDto } from "./dto/update-product.dto";
+import { ProductsService } from "./products.service";
+import { Product } from "./schemas/product.schema";
 
 // express
 // app.use((req, res, next) => {
 //   res.status(201).end('Poka')
 // })
 
-@Controller('products')
+@Controller("products")
 export class ProductsController {
-
-  constructor(private readonly productsService: ProductsService) {
-  }
+  constructor(private readonly productsService: ProductsService) {}
 
   // @Get()
   // // @Redirect('https://google.com', 301)
@@ -35,29 +33,32 @@ export class ProductsController {
 
   @Get()
   getAll(): Promise<Product[]> {
-    return this.productsService.getAll()
+    return this.productsService.getAll();
   }
 
-  @Get(':id')
-  getOne(@Param('id') id: string): Promise<Product> {
-    return this.productsService.getById(id)
+  @Get(":id")
+  getOne(@Param("id") id: string): Promise<Product> {
+    return this.productsService.getById(id);
   }
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  @Header('Cache-Control', 'none')
+  @Header("Cache-Control", "none")
+  @Header("Access-Control-Allow-Origin", "https://bekor.vercel.app")
   create(@Body() createProductDto: CreateProductDto): Promise<Product> {
-    return this.productsService.create(createProductDto)
+    return this.productsService.create(createProductDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string): Promise<Product> {
-    return this.productsService.remove(id)
+  @Delete(":id")
+  remove(@Param("id") id: string): Promise<Product> {
+    return this.productsService.remove(id);
   }
 
-  @Put(':id')
-  update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string): Promise<Product> {
-    return this.productsService.update(id, updateProductDto)
+  @Put(":id")
+  update(
+    @Body() updateProductDto: UpdateProductDto,
+    @Param("id") id: string
+  ): Promise<Product> {
+    return this.productsService.update(id, updateProductDto);
   }
-
 }
